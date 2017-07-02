2ac1f811124321f59cc98e9c5206dddd63efe87e
eric-weaver <eric@canary.is>
2017-07-02 14:33:50 -0400

    pubnub demo

--- ac_remote/main.py
+++ ac_remote/main.py
@@ -1,8 +1,43 @@
 from vendored.lirc import Lirc
+from pubnub.callbacks import SubscribeCallback
+from pubnub.enums import PNStatusCategory
+from pubnub.pnconfiguration import PNConfiguration
+from pubnub.pubnub import PubNub
 
+pnconfig = PNConfiguration()
+pnconfig.subscribe_key = "<subscribe_key>"
+pnconfig.ssl = False
+
+pubnub = PubNub(pnconfig)
 
 lirc = Lirc()
 
-print ", ".join(lirc_obj.devices())
 
-lirc_obj.send_once('frigidaireAC', 'KEY_DOWN')
+class MySubscribeCallback(SubscribeCallback):
+    def presence(self, pubnub, presence):
+        pass  # handle incoming presence data
+
+    def status(self, pubnub, status):
+        if status.category == PNStatusCategory.PNUnexpectedDisconnectCategory:
+            pass  # This event happens when radio / connectivity is lost
+
+        elif status.category == PNStatusCategory.PNConnectedCategory:
+            # Connect event. You can do stuff like publish, and know you'll get it.
+            # Or just use the connected event to confirm you are subscribed for
+            # UI / internal notifications, etc
+            print('Connected')
+        elif status.category == PNStatusCategory.PNReconnectedCategory:
+            pass
+            # Happens as part of our regular operation. This event happens when
+            # radio / connectivity is lost, then regained.
+        elif status.category == PNStatusCategory.PNDecryptionErrorCategory:
+            pass
+            # Handle message decryption error. Probably client configured to
+            # encrypt messages and on live data feed it received plain text.
+
+    def message(self, pubnub, message):
+        lirc.send_once('frigidaireAC', message.message)
+
+
+pubnub.add_listener(MySubscribeCallback())
+pubnub.subscribe().channels('test').execute()
